bd5aaa19a75d221ca0ef8ba8d2eb3cf6d2ff0b79
pigrabbit87 <pigrabbit87@yahoo.com.tw>
2015-04-12 14:34:39 -0700

    First rails app

---
+++ testbank/app/controllers/homepage_controller.rb
@@ -0,0 +1,16 @@
+class HomepageController < ApplicationController
+	def index 
+	end
+
+	def log_in
+	end
+
+	def sign_up
+	end
+
+	def about
+	end
+
+	def contact
+	end
+end
---
+++ testbank/test/controllers/homepage_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class HomepageControllerTest < ActionController::TestCase
+  test "homepage link" do
+
+  end
+
+  test "sign up link" do
+  end
+
+  test "log in link" do
+  end
+
+  test "account popup" do
+  end
+end
