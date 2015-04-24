bfc442a858444a614548943687570b27d78ef51b
Darin Minamoto <darinm223@gmail.com>
2015-04-23 21:17:15 -0700

    Added name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :first_name, :scope => [:last_name] # (first_name, last_name) have to be unique
 end
