00b707de1e215ae2c55f99bc954c21172ac1371f
pigrabbit87 <pigrabbit87@yahoo.com.tw>
2015-05-01 13:35:21 -0700

    change migration description, text to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428204133) do
+ActiveRecord::Schema.define(version: 20150501203243) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "department"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150428204133) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "document"
-    t.text     "description", default: "",  null: false
     t.string   "year",        default: "",  null: false
     t.string   "course_id",   default: "1", null: false
+    t.string   "description", default: ""
   end
 
   create_table "users", force: :cascade do |t|
