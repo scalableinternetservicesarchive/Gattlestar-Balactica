a4c45588e5a2aa3b5adcbddecd03127ee4bb8ea0
EmilyHwang <emily.hwang94@gmail.com>
2015-05-02 22:45:15 -0700

    validating course before adding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,21 +22,49 @@ class UsersController < ApplicationController
   def add_course
   end
 
+  def check_duplicate(dept, course)
+    @courses_string = current_user.courses_taken.split(",")
+
+    @courses_string.each do |course|
+      course_id_start = course.rindex(' ')
+      if course_id_start != nil 
+        @dpm_name = course.slice(0, course_id_start)
+        @course_id = course[course_id_start + 1..-1]
+
+        if @dpm_name == dept && @course_id == course
+          return true
+        end
+      end
+    end
+  end
+
   def add_course_post
      @department = params[:dep_name]
+      @course = params[:crse_name]
+
     if @department == nil || @department.empty?
-       return redirect_to root_path, flash: {alert: 'Please Enter A Valid Department Name'}
+       return redirect_to :back, flash: {alert: 'Please Enter A Valid Department Name'}
     end
 
-    @course = params[:crse_name]
     if @course == nil || @course.empty?
-      return redirect_to root_path, flash: {alert: 'Please Enter A Valid Course Name'}
+      return redirect_to :back, flash: {alert: 'Please Enter A Valid Course Name'}
+    end
+
+    #check if class in database
+    @query_results = Course.search_by_course(@department, @course)
+    if @query_results == nil || @query_results.empty?
+      return redirect_to :back, flash: {alert: 'Please Enter a Valid Course Name'}
+    end
+    
+    #check if already added class
+    if check_duplicate(@department, @course)
+      return redirect_to :back, flash: {alert: 'Class Already Added'}
     else
       @courses_taken = User.find(current_user.id).courses_taken
       if current_user.update_attribute("courses_taken", @courses_taken + ", #{@department} #{@course}")
-           redirect_to current_user, :notice  => "Successfully Added Course"
-      else
+        return redirect_to current_user, :notice  => "Successfully Added Course"
       end
     end
+
   end
 end
