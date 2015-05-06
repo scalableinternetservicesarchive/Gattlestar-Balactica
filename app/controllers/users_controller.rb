95a1d12503782a027116a7dcf5c533bf93371d43
EmilyHwang <emily.hwang94@gmail.com>
2015-05-06 12:34:32 -0700

    fixing add_course bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def add_course_taken
   end
 
-  def check_duplicate(dept, course)
+  def check_duplicate(dept, crse)
     @courses_string = current_user.courses_taken.split(",")
 
     @courses_string.each do |course|
@@ -30,17 +30,17 @@ class UsersController < ApplicationController
       if course_id_start != nil 
         @dpm_name = course.slice(0, course_id_start)
         @course_id = course[course_id_start + 1..-1]
-
-        if @dpm_name == dept && @course_id == course
+        if @dpm_name == dept && @course_id == crse
           return true
         end
       end
     end
+    return false
   end
 
   def add_course_taken_post
-     @department = params[:dep_name]
-      @course = params[:crse_name]
+    @department = params[:dep_name]
+    @course = params[:crse_name]
 
     if @department == nil || @department.empty?
        return redirect_to :back, flash: {alert: 'Please Enter A Valid Department Name'}
@@ -55,13 +55,13 @@ class UsersController < ApplicationController
     if @query_results == nil || @query_results.empty?
       return redirect_to :back, flash: {alert: 'Please Enter a Valid Course Name'}
     end
-    
+
     #check if already added class
     if check_duplicate(@department, @course)
       return redirect_to :back, flash: {alert: 'Class Already Added'}
     else
       @courses_taken = User.find(current_user.id).courses_taken
-      if current_user.update_attribute("courses_taken", @courses_taken + ", #{@department} #{@course}")
+      if current_user.update_attribute("courses_taken", @courses_taken + ",#{@department} #{@course}")
         return redirect_to current_user, :notice  => "Successfully Added Course to Courses Taken"
       end
     end
