37431f7bb4d9c3e8aaa235813a819baa9ade834a
EmilyHwang <emily.hwang94@gmail.com>
2015-05-10 19:51:58 -0700

    adding pagination

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'carrierwave'
 
 gem 'fog-aws'
 
+gem 'will_paginate'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,18 @@
 class HomepageController < ApplicationController
 	def index 
-		@course_hash= Course.find_all_courses
+    @course_hash= Course.find_all_courses
+
+    current_page = params[:page].to_i
+    if current_page == 0
+      current_page = 1
+    end
+    per_page = params[:per_page] || 20
+    total = params[:total_entries] || @course_hash.length
+
+    @paginated_courses = WillPaginate::Collection.create(current_page, per_page, total) do |pager|
+      start = (current_page-1)*per_page # assuming current_page is 1 based.
+      pager.replace(@course_hash.keys[start, per_page])
+    end
 	end
 
   def log_in
