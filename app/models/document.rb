b859e92e3b45ed24de787a00a09448ebc4c906d2
Darin Minamoto <darinm223@gmail.com>
2015-05-12 14:40:05 -0700

    Added some document validations and remove user name validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,16 +5,16 @@ class Document < ActiveRecord::Base
 
   attr_accessor :is_thumbnable
 
-  #validates :document, :presence => true, :file_size => {
-  #  :maximum => 0.5.megabytes.to_i
-  #}
+  validates :document, :presence => true, :file_size => {
+    :maximum => 10.0.megabytes.to_i
+  }
 
+  validates :description, :presence => true
 
   # to get path to uploaded file for @doc do @doc.document.current_path
   # to get file extension for uploaded file do @doc.document.file.extension.downcase
 
   def display_test
-
   end
 
   def format_quarter_year
