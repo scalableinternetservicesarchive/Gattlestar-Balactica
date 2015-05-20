5fa1ad0602d50c43dc4996d720ca2ea3d0e169c1
EmilyHwang <emily.hwang94@gmail.com>
2015-05-19 20:16:26 -0700

    adding email confirmation, only allowing @ucla.edu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: :login,
+    user_name: "uclatestbank188@gmail.com",
+    password: "p@ssword1234"
+  }
 end
