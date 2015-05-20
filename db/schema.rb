7f3996385ac207c971550a4e731c62f1e680c2b9
EmilyHwang <emily.hwang94@gmail.com>
2015-05-19 21:03:41 -0700

    bypassing confirmation for admin, adding placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504202831) do
+ActiveRecord::Schema.define(version: 20150520004844) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "department"
@@ -53,8 +53,12 @@ ActiveRecord::Schema.define(version: 20150504202831) do
     t.string   "documents_uploaded"
     t.integer  "credits",                default: 0
     t.boolean  "admin",                  default: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
